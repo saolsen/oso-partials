95624c01bec6004c1d819df726354211823f23ba
steve olsen <dev@steve.computer>
2021-01-05 14:27:13 -0600

    before adding builtins

--- app.py
+++ app.py
@@ -3,63 +3,117 @@ from polar import Variable, Expression
 
 from flask import Flask, render_template
 
-sdf_header = """
-float sdf(vec3 p) {
-    float x = p.x;
-    float y = p.y;
-    float z = p.z;
 
-"""
+def compile_sdf(bindings):
+    cases = []
+    for i, b in enumerate(bindings):
+        x_exp = b["x"]
+        x_body = compile_body(x_exp, "x")
+        y_exp = b["y"]
+        y_body = compile_body(y_exp, "y")
+        z_exp = b["z"]
+        z_body = compile_body(z_exp, "z")
+        ans_exp = b["ans"]
+        ans_body = compile_body(ans_exp, "ans")
+        case = f"        case {i}: {{\n{x_body}{y_body}{z_body}{ans_body}            result = min(result, ans);\n        }}"
+        cases.append(case)
+    case_statements = "\n".join(cases)
+    switch_statement = f"        switch (i) {{\n{case_statements}\n        default: break;\n        }}\n"
+    loop = f"    for(int i=0; i<{len(bindings)}; i++) {{\n{switch_statement}    }}\n"
+    sdf_function = f"float sdf(vec3 p) {{\n    float result = 3.402823e+38;\n    float x = p.x;\n    float y = p.y;\n    float z = p.z;\n{loop}    return result;\n}}\n"
+    return sdf_function
 
-sdf_footer = """
-    return _this;
-}
-"""
+
+def compile_body(exp, this_var):
+    if isinstance(exp, float):
+        if this_var == "ans":
+            return f"            float ans = {exp};\n"
+        else:
+            return f"            if (!({this_var} == {exp})) {{ break; }}\n"
+    elif isinstance(exp, Variable):
+        return ""
+    else:
+        assert isinstance(exp, Expression)
+        body = compile(exp, this_var)
+        if isinstance(body, list):
+            body = "            " + "            ".join(body)
+        else:
+            body = "            " + body
+        return body
 
 
-def compile(exp):
+def compile(exp, this_var):
+    if isinstance(exp, int):
+        raise Exception("No ints, only floats")
     if isinstance(exp, float):
         return str(exp)
     if isinstance(exp, Variable):
         # This is incredibly stupid, we need a way to get a variables name...
-        return exp.split()[0]
+        name = exp.split()[0]
+        if name not in ["x", "y", "z", "_this"]:
+            name = "_" + this_var + name
+        if name == "_this":
+            return this_var
+        return name
     elif isinstance(exp, Expression):
         if exp.operator == "And":
-            args = [compile(arg) for arg in exp.args]
-            sdf_body = "    " + "    ".join(args)
-
-            return f"{sdf_header}{sdf_body}{sdf_footer}"
+            args = [compile(arg, this_var) for arg in exp.args]
+            return args
 
         elif exp.operator == "Mul":
             assert len(exp.args) == 3
-            lhs = compile(exp.args[0])
-            rhs = compile(exp.args[1])
+            lhs = compile(exp.args[0], this_var)
+            rhs = compile(exp.args[1], this_var)
             assert isinstance(exp.args[2], Variable)
-            result = compile(exp.args[2])
+            result = compile(exp.args[2], this_var)
             return f"float {result} = {lhs} * {rhs};\n"
 
         elif exp.operator == "Add":
             assert len(exp.args) == 3
-            lhs = compile(exp.args[0])
-            rhs = compile(exp.args[1])
+            lhs = compile(exp.args[0], this_var)
+            rhs = compile(exp.args[1], this_var)
             assert isinstance(exp.args[2], Variable)
-            result = compile(exp.args[2])
+            result = compile(exp.args[2], this_var)
             return f"float {result} = {lhs} + {rhs};\n"
 
         elif exp.operator == "Sub":
             assert len(exp.args) == 3
-            lhs = compile(exp.args[0])
-            rhs = compile(exp.args[1])
+            lhs = compile(exp.args[0], this_var)
+            rhs = compile(exp.args[1], this_var)
             assert isinstance(exp.args[2], Variable)
-            result = compile(exp.args[2])
+            result = compile(exp.args[2], this_var)
             return f"float {result} = {lhs} - {rhs};\n"
 
         elif exp.operator == "Sqrt":
             assert len(exp.args) == 2
-            arg = compile(exp.args[0])
+            arg = compile(exp.args[0], this_var)
             assert isinstance(exp.args[1], Variable)
-            result = compile(exp.args[1])
+            result = compile(exp.args[1], this_var)
             return f"float {result} = sqrt({arg});\n"
+
+        elif exp.operator == "Geq":
+            assert len(exp.args) == 2
+            lhs = compile(exp.args[0], this_var)
+            rhs = compile(exp.args[1], this_var)
+            return f"if (!({lhs} >= {rhs})) {{ break; }}\n"
+
+        elif exp.operator == "Leq":
+            assert len(exp.args) == 2
+            lhs = compile(exp.args[0], this_var)
+            rhs = compile(exp.args[1], this_var)
+            return f"if (!({lhs} <= {rhs})) {{ break; }}\n"
+
+        elif exp.operator == "Gt":
+            assert len(exp.args) == 2
+            lhs = compile(exp.args[0], this_var)
+            rhs = compile(exp.args[1], this_var)
+            return f"if (!({lhs} > {rhs})) {{ break; }}\n"
+
+        elif exp.operator == "Lt":
+            assert len(exp.args) == 2
+            lhs = compile(exp.args[0], this_var)
+            rhs = compile(exp.args[1], this_var)
+            return f"if (!({lhs} < {rhs})) {{ break; }}\n"
 
         else:
             raise Exception("Unhandled operator")
@@ -69,42 +123,18 @@ def compile(exp):
 
 app = Flask(__name__)
 
+print("compiling")
+oso = Oso()
+oso.load_file("scene.polar")
+
+results = list(
+    oso.query_rule("sdf", Variable("x"), Variable("y"), Variable("z"), Variable("ans"))
+)
+expressions = [result["bindings"] for result in results]
+sdf_function = compile_sdf(expressions)
+print(sdf_function)
+
 
 @app.route("/")
 def index():
-    oso = Oso()
-    oso.load_file("scene.polar")
-
-    results = list(
-        oso.query_rule(
-            "sdf", Variable("x"), Variable("y"), Variable("z"), Variable("ans")
-        )
-    )
-    expressions = [result["bindings"]["ans"] for result in results]
-    assert len(expressions) == 1
-
-    sdf_function = compile(expressions[0])
-    print(sdf_function)
-
-    # sdf_function = """
-    # float sphere(vec3 pos, vec3 center, float radius) {
-    #     return length(pos - center) - radius;
-    # }
-
-    # float box(vec3 pos, vec3 center, vec3 size, float corner) {
-    #     return length(max(abs(pos-center)-size, 0.0))-corner;
-    # }
-
-    # float unite(float a, float b) { return min(a,b); }
-    # float subtract(float a, float b) { return max(-a,b); }
-    # float intersect(float a, float b) { return max(a,b); }
-
-    # // This is what we're going to generate from the polar partial.
-    # float sdf(vec3 p) {
-    #     float s = sphere(p, vec3(0.), 1.25);
-    #     float b = box(p, vec3(0.), vec3(1.), 0.);
-    #     return subtract(s, b);
-    # }
-    # """
-
     return render_template("app.html", sdf_function=sdf_function)
--- scene.polar
+++ scene.polar
@@ -1,7 +1,66 @@
+# new builtins, they look like rules but they aint they return values
+# yeah it's confusing, sorry.
+# sqrt(f)
+# min(a,b)
+# max(a,b)
+# sign(f)
+
+# abs is a glsl builtin and could use that
+abs(x,ax) if x >= 0.0 and ax = x;
+abs(x,ax) if x < 0.0 and ax = x * -1.0;
+abs(x,y,z,ax,ay,az) if
+    abs(x,ax) and
+    abs(y,ay) and
+    abs(z,az);
+
+min(x,y,ans) if x <= y and ans = x;
+min(x,y,ans) if x > y and ans = y;
+min(ax,ay,az,bx,by,bz,mx,my,mz) if
+    min(ax,bx,mx) and
+    min(ay,by,my) and
+    min(az,bz,mz);
+
+max(x,y,ans) if x >= y and ans = x;
+max(x,y,ans) if x < y and ans = y;
+max(ax,ay,az,bx,by,bz,mx,my,mz) if
+    max(ax,bx,mx) and
+    max(ay,by,my) and
+    max(az,bz,mz);
+
+?= max(
+    1,3,1,
+    4,-2,1,
+    4,3,1);
+
+sub(ax,ay,az,bx,by,bz,rx,ry,rz) if
+    rx = ax - bx and
+    ry = ay - by and
+    rz = az - bz;
+
+box(x,y,z,bx,by,bz,ans) if
+    abs(x,y,z,ax,ay,az) and
+    sub(ax,ay,az,bx,by,bz,qx,qy,qz) and
+    max(qx,qy,qz,0.0,0.0,0.0,mqx,mqy,mqz) and
+    len(mqx,mqy,mqz,l) and
+    max(qy,qz,m) and
+    max(qx,m,mm) and
+    min(mm,0.0,mmm) and
+    ans = l + mmm;
+
+sdf(x,y,z,ans) if box(x,y,z,1.0,1.0,1.0,ans);
+
 # The sdf function returns the distance from a point x,y,z to a surface.
 len(x, y, z, ans) if ans = sqrt(x*x + y*y + z*z);
 
-# A unit sphere
-sdf(x, y, z, ans) if
-    len(x,y,z,len) and
-    ans = len - 1.0;
+sphere(x, y, z, cx, cy, cz, radius, ans) if
+    sub(x,y,z,cx,cy,cz,ox,oy,oz) and
+    len(ox,oy,oz, l) and
+    ans = l - radius;
+
+#sdf(x,y,z,ans) if sphere(x,y,z,1.0,1.0,0.0,1.25,ans);
+#sdf(x,y,z,ans) if sphere(x,y,z,-1.0,1.0,0.0,1.25,ans);
+
+# sdf(x,y,z,ans) if x < 0.0 and ans = 1.0;
+# sdf(x,y,z,ans) if x >= 0.0 and ans = 2.0;
+
+# NOTE: Don't cut lol
